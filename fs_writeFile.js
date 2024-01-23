f74e529f7bcc305172f7d425949c54348e867ed8
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-01-23 21:32:16 +0530

    commit

--- fs_writeFile.js
+++ fs_writeFile.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const data = 'hello my name is himanshu and i am great!!';
+const data2 = 'this world is great!!!';
 
 fs.writeFile('./myFolder/myFile.txt', data, (err)=>{
     if(err){
@@ -11,6 +12,16 @@ fs.writeFile('./myFolder/myFile.txt', data, (err)=>{
         console.log('Writen to file successfully!!');
     }
 })
+fs.writeFileSync('./myFolder/myFile.txt', data2, (err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    else{
+        console.log('Writen to file successfully 2nd time!!!!!');
+    }
+})
 
-console.log('hello world no. 1');
-console.log('hello world no. 2');
+
+// console.log('hello world no. 1');
+// console.log('hello world no. 2');
