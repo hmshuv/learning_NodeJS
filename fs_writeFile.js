4fb84dad90ef94a70f98a6edaeef0a604dd26151
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-01-23 20:00:01 +0530

    commit

---
+++ fs_writeFile.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const data = 'hello my name is himanshu and i am great!!';
+
+fs.writeFile('./myFolder/myFile.txt', data, (err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    else{
+        console.log('Writen to file successfully!!');
+    }
+})
