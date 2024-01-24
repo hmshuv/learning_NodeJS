524cb730fecf3b00e652a40f0c534ba32967937c
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-01-25 02:48:19 +0530

    commit

---
+++ fs_rename.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.rename('./myFolder/myFile.txt', './myFolder/myFileAsync.txt', (err) => {
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log('File renamed successfully!');
+})
