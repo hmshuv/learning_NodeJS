a5ca06f2ebda90782dbf04296d9a13da0914457c
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-01-26 22:38:59 +0530

    commit

--- http_server.js
+++ http_server.js
@@ -1,8 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+
+    if(req.url === '/'){
+        res.end('This is my home page :)');
+    }
+    else if(req.url === '/about'){
+        res.end('This is about page.');
+    }
+    else if(req.url === '/contact'){
+        res.end('This is contact page.');
+    }
+    else{
+        res.end('404 page not found :(');
+    }
     console.log('New req rec');
-    res.end('hello world!!');
+    
 });
 
 server.listen(8080, () => console.log('server is listening at port 8080'));
