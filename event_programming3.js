7717fdb9e9cd2a0919ea5fff6aac89f451ae17ea
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-01-28 01:24:23 +0530

    commit

---
+++ event_programming3.js
@@ -0,0 +1,11 @@
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter;
+
+const greetBirthday = (name, newAge) => {
+    // name = Himanshu Gupta
+    // newAge = 20
+    console.log(`Happy Birthday ${name}! You are now ${newAge}!! `);
+}
+myEmitter.on('birthdayEvent', greetBirthday);
+
+myEmitter.emit('birthdayEvent', 'Himanshu Gupta', '20');
