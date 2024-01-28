fccc98f07a07d9d35fbc83d518e8abbe82eddcfc
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-01-28 21:30:18 +0530

    commit

---
+++ post.js
@@ -0,0 +1,15 @@
+const messagingApp = new EventEmitter();
+
+// User joins event
+messagingApp.on('userJoins', (userName) => {
+    console.log(`${userName} has joined the chat!`);
+});
+
+// New message event
+messagingApp.on('newMessage', (message) => {
+    console.log(`New message: ${message}`);
+});
+
+// Sending events
+messagingApp.emit('userJoins', 'JohnDoe');
+messagingApp.emit('newMessage', 'Hello everyone!');
